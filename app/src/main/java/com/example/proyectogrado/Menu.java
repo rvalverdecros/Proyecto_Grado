6e93527878f115ea36b038323d25463bdd6513f6
rafa <rvalcro022@g.educaand.es>
2023-04-22 17:39:01 +0200

    Mas Botones

--- app/src/main/java/com/example/proyectogrado/Menu.java
+++ app/src/main/java/com/example/proyectogrado/Menu.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
@@ -17,7 +18,8 @@ public class Menu extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
 
-    Button cerrarSesion;
+    TextView mipuntuaciontxt,uid,correo,nombre,menutxt;
+    Button jugarbtn,puntuacionesbtn,acercadebtn,cerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +29,38 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
-        cerrarSesion = findViewById(R.id.cerrarSesion);
 
+        mipuntuaciontxt = findViewById(R.id.mipuntuaciontxt);
+        uid = findViewById(R.id.uid);
+        correo = findViewById(R.id.correo);
+        nombre = findViewById(R.id.nombre);
+        menutxt = findViewById(R.id.menutxt);
+
+        jugarbtn = findViewById(R.id.jugarbtn);
+        puntuacionesbtn = findViewById(R.id.puntuacionesbtn);
+        acercadebtn = findViewById(R.id.acercadebtn);
+        cerrarSesion = findViewById(R.id.cerrarSesionbtn);
+
+        jugarbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this, "JUGAR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        puntuacionesbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this, "PUNTUACIONES", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        acercadebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Menu.this, "ACERCA DE", Toast.LENGTH_SHORT).show();
+            }
+        });
         cerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
